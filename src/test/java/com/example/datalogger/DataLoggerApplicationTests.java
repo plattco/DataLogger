aa69732907bc995545c958741eaaf0675f52e00c
Cody Platt <62489180+plattco@users.noreply.github.com>
2025-09-03 13:44:08 -0400

    Creating new DataLogger

---
+++ src/test/java/com/example/datalogger/DataLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datalogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
